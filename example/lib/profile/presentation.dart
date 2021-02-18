7e9ca4dd3da6d357ac2a16e35d91ae016061dc52
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-02-18 06:07:36 +0300

    WIP some improves in profile UI

--- example/lib/profile/presentation.dart
+++ example/lib/profile/presentation.dart
@@ -35,50 +35,126 @@ class _UserDetailsState extends State<UserDetailsPage> {
             );
           }
 
-          return UserDetailsWidget(state.value);
+          return UserDetailsWidget(
+              state.value, () => _bloc.add(UserDetailsEvent.increaseBalance()));
         });
   }
 }
 
 class UserDetailsWidget extends StatelessWidget {
   final UserDetailsModel _model;
+  final Function() _onClickListener;
 
-  const UserDetailsWidget(this._model);
+  const UserDetailsWidget(this._model, this._onClickListener);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Container(decoration: BoxDecoration(color: Colors.black12),child: Stack(
       children: [
-        FractionallySizedBox(
-          alignment: Alignment.topCenter,
-          heightFactor: 0.35,
-          child: Stack(
-            children: [
-              CachedNetworkImage(
-                  imageUrl: _model.backgroundUrl,
-                  imageBuilder: (context, imageProvider) => Container(
-                        width: double.maxFinite,
-                        height: double.maxFinite,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: imageProvider, fit: BoxFit.fill)),
-                      )),
-              Align(
-                  alignment: Alignment.center,
-                  child: CachedNetworkImage(
-                      imageUrl: _model.thumbnailUrl,
+        Align(
+            alignment: Alignment.topCenter,
+            child: FractionallySizedBox(
+              heightFactor: 0.41,
+              child: Stack(
+                children: [
+                  CachedNetworkImage(
+                      imageUrl: _model.backgroundUrl,
                       imageBuilder: (context, imageProvider) => Container(
-                            width: 80.0,
-                            height: 80.0,
+                            width: double.maxFinite,
+                            height: double.maxFinite,
                             decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                  image: imageProvider, fit: BoxFit.cover),
-                            ),
-                          )))
-            ],
-          ),
-        ),
+                                image: DecorationImage(
+                                    image: imageProvider, fit: BoxFit.fill)),
+                          )),
+                  Align(
+                      alignment: Alignment.center,
+                      child: _createDetailsWidget())
+                ],
+              ),
+            )),
+        Align(
+          child: FractionallySizedBox(
+              alignment: Alignment.bottomCenter,
+              heightFactor: 0.61,
+              child: Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: SizedBox.expand(
+                      child: Container(
+                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(16), topRight: Radius.circular(16))))))),
+          alignment: Alignment.bottomCenter,
+        )
+      ],
+    ));
+  }
+
+  Widget _createDetailsWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+            padding: EdgeInsets.all(5),
+            child: CachedNetworkImage(
+                imageUrl: _model.thumbnailUrl,
+                imageBuilder: (context, imageProvider) => Container(
+                      width: 80.0,
+                      height: 80.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: imageProvider, fit: BoxFit.cover),
+                      ),
+                    ))),
+        Padding(
+            child: Text(_model.displayName,
+                style: TextStyle(fontSize: 12, color: Colors.white)),
+            padding: EdgeInsets.all(5)),
+        Padding(
+            child: Text("${_model.balance} \$",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold)),
+            padding: EdgeInsets.all(5)),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Flexible(
+              child: SizedBox(
+                child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                      color: Colors.black,
+                      child: Text(
+                        "Пополнить Баланс",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () => _onClickListener.call(),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.blueAccent)),
+                    )),
+                width: double.infinity,
+              ),
+              flex: 1,
+            ),
+            Flexible(
+                child: SizedBox(
+                  child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: FlatButton(
+                        child: Text("Вывод средств",
+                            style: TextStyle(color: Colors.white)),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.white)),
+                      )),
+                  width: double.infinity,
+                ),
+                flex: 1),
+          ],
+        )
       ],
     );
   }
