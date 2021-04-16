56282df6cf5dd9b927894e1ebd9a0b6d54449f89
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-04-16 08:00:08 +0200

    WIP: Update http library

--- lib/config/model.dart
+++ lib/config/model.dart
@@ -1,6 +1,6 @@
 
 
-import 'package:applithium_core/services/ads/service.dart';
+import 'package:applithium_core/services/ads/model.dart';
 import 'package:applithium_core/services/promo/service.dart';
 
 abstract class Configuration {
