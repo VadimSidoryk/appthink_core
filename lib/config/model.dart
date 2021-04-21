86d459589440bbe337f9d2bdfce6e8104234986f
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-04-21 07:03:36 +0200

    WIP: Some minor changes in config model

--- lib/config/model.dart
+++ lib/config/model.dart
@@ -4,9 +4,14 @@ import 'package:applithium_core/services/ads/model.dart';
 import 'package:applithium_core/services/promo/service.dart';
 
 abstract class Configuration {
-  String preferencesName;
 
-  PromoConfig promoConfig;
-  AdsConfig adsConfig;
+  String get preferencesName;
 
+  String getString(dynamic key);
+
+  String getVector(dynamic key);
+
+  PromoConfig get promoConfig;
+
+  AdsConfig get adsConfig;
 }
