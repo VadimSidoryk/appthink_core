6c245721802d970a1f5a40edd039df1fdb25091e
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-06-12 05:15:17 +0200

    WIP: add base firebase_core module

---
+++ core_firebase/lib/analytics.dart
@@ -0,0 +1,22 @@
+import 'package:applithium_core/services/analytics/analyst.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:flutter/src/widgets/navigator.dart';
+
+class FirebaseAnalyst extends Analyst {
+
+  final FirebaseAnalytics _analytics = FirebaseAnalytics();
+
+  @override
+  NavigatorObserver get navigatorObserver => FirebaseAnalyticsObserver(analytics: _analytics);
+
+  @override
+  void setUserProperty(String name, Object? value) {
+    _analytics.setUserProperty(name: name, value: value?.toString());
+  }
+
+  @override
+  void trackEvent({required String name, Map<String, Object>? params}) {
+    _analytics.logEvent(name: name, parameters: params);
+  }
+}
