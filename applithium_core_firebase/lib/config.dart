184322bc037301093bd75e1fcd187e5c829c41b9
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-08-02 06:54:55 +0200

    WIP: add applithium_core_firebase to example module

--- applithium_core_firebase/lib/config.dart
+++ applithium_core_firebase/lib/config.dart
@@ -1,6 +1,5 @@
 import 'package:applithium_core/config/model.dart';
 import 'package:applithium_core/config/provider.dart';
-import 'package:applithium_core/services/resources/model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 
