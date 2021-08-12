b998d4097ed98ba5c9e58cd89ee7e45832fac1ce
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-08-12 07:29:25 +0300

    Force fetch remote config, add logs to Firebase analytics and config

--- applithium_core_firebase/lib/analytics.dart
+++ applithium_core_firebase/lib/analytics.dart
@@ -3,6 +3,7 @@ import 'package:applithium_core/services/analytics/analyst.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/src/widgets/navigator.dart';
+import 'package:applithium_core/logs/extension.dart';
 
 class FirebaseAnalyst extends Analyst {
 
@@ -13,11 +14,13 @@ class FirebaseAnalyst extends Analyst {
 
   @override
   void setUserProperty(String name, Object? value) {
+    logMethod("setUserProperty", params: [name , value]);
     _analytics.setUserProperty(name: name, value: value?.toString());
   }
 
   @override
   void onNewEvent(AplEvent event) {
+    logMethod("onNewEvent", params: [event]);
     _analytics.logEvent(name: event.name, parameters: event.params);
   }
 }
--- applithium_core_firebase/lib/config.dart
+++ applithium_core_firebase/lib/config.dart
@@ -3,6 +3,7 @@ import 'package:applithium_core/config/provider.dart';
 import 'package:applithium_core/services/localization/config.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:applithium_core/logs/extension.dart';
 
 const FIREBASE_CONFIG_RESOURCES_KEY = "resources";
 const FIREBASE_CONFIG_EVENTS_KEY = "event_handlers";
@@ -17,14 +18,18 @@ class FirebaseConfigProvider extends ConfigProvider {
 
   @override
   Future<AplConfig> getApplicationConfig() async {
+    logMethod("getApplicationConfig");
     await Firebase.initializeApp();
     final RemoteConfig remoteConfig = RemoteConfig.instance;
     await remoteConfig.setConfigSettings(RemoteConfigSettings(
       fetchTimeout: Duration(seconds: fetchTimeoutSec),
       minimumFetchInterval: const Duration(hours: 1),
     ));
-    await remoteConfig.setDefaults(defaults);
+    // await remoteConfig.setDefaults(defaults);
+    await remoteConfig.fetchAndActivate();
     RemoteConfigValue(null, ValueSource.valueStatic);
+    final testValue = remoteConfig.getString("test_value");
+    log("remoteConfig = $testValue");
 
     return AplConfig(
       messagingApiKey: "",
