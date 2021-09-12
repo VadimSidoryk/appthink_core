241a62fc68c7df758e42057b4d22ea7813cbc415
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-09-12 12:00:34 +0200

    WIP: incomplete systemListener

--- lib/events/event_bus.dart
+++ lib/events/event_bus.dart
@@ -1,5 +1,6 @@
 import 'package:applithium_core/domain/supervisor.dart';
 import 'package:applithium_core/events/base_event.dart';
+import 'package:applithium_core/events/system_listener.dart';
 import 'package:applithium_core/logs/extension.dart';
 import 'package:applithium_core/services/analytics/bloc_adapter.dart';
 import 'package:flutter/widgets.dart';
@@ -7,13 +8,13 @@ import 'package:flutter/widgets.dart';
 import 'events_listener.dart';
 
 class EventBus {
-  final Set<EventsListener> listeners;
+  final Set<SystemListener> listeners;
 
   EventBus({required this.listeners});
 
   List<NavigatorObserver> get navigatorObservers {
     return listeners
-        .map((impl) => impl.navigatorObservers)
+        .map((impl) => impl.navigatorObserver)
         .reduce((result, currentList) => result + currentList)
         .toList();
   }
