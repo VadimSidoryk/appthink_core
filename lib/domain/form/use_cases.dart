a6c26cc28aaee0c031185bc3e7dbf37d7399dd7f
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-09-22 08:02:04 +0200

    Added Form useCases

---
+++ lib/domain/form/use_cases.dart
@@ -0,0 +1,10 @@
+import 'package:applithium_core/domain/content/use_cases.dart';
+import 'package:applithium_core/usecases/base.dart';
+
+class FormUseCases<M> extends ContentUseCases<M> {
+
+  final UseCase<M, void> post;
+
+  FormUseCases({required this.post, required UseCase<void, M> load, required UseCase<M, M> update}) : super(load: load, update: update);
+
+}
