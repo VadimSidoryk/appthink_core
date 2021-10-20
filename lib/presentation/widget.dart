a6bf58fa4824e58b2ca871fac8d6be1f43fdb93f
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-10-20 08:23:09 +0200

    Cached dependencies updated

--- lib/presentation/widget.dart
+++ lib/presentation/widget.dart
@@ -9,14 +9,30 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'events.dart';
 
-abstract class StateWithBloc<W extends StatefulWidget, S extends BaseState> extends State<W>
-    implements EventsListener {
+typedef BlocBuilder<S extends BaseState> = Bloc<WidgetEvents, S> Function(
+    BuildContext);
+typedef WidgetWithStateBuilder<S extends BaseState> = Widget Function(S);
+
+abstract class StateWithBloc<W extends StatefulWidget, S extends BaseState>
+    extends State<W> implements EventsListener {
+
+  static StateWithBloc<W, S>
+  fromBuilders<W extends StatefulWidget, S extends BaseState>(
+      BlocBuilder<S> blocBuilder, WidgetWithStateBuilder<S> widgetBuilder) {
+    return _UtilStateWithBloc(blocBuilder, widgetBuilder);
+  }
+
+  static StateWithBloc<W, S>
+  fromBloc<W extends StatefulWidget, S extends BaseState>(
+      Bloc<WidgetEvents, S> bloc, WidgetWithStateBuilder<S> widgetBuilder) {
+    return _UtilStateWithBloc((context) => bloc, widgetBuilder);
+  }
+
   late final Bloc<WidgetEvents, S> _bloc;
 
   @override
   void initState() {
-    _bloc = createBloc(context)
-      ..add(BaseWidgetEvents.widgetCreated(widget));
+    _bloc = createBloc(context)..add(BaseWidgetEvents.widgetCreated(widget));
     super.initState();
   }
 
@@ -31,8 +47,7 @@ abstract class StateWithBloc<W extends StatefulWidget, S extends BaseState> exte
     return BlocConsumer<Bloc<WidgetEvents, S>, S>(
       bloc: _bloc,
       builder: (context, state) => createWidgetForState(state),
-      listener: (context, state) {
-      },
+      listener: (context, state) {},
     );
   }
 
@@ -42,5 +57,23 @@ abstract class StateWithBloc<W extends StatefulWidget, S extends BaseState> exte
     if (event is WidgetEvents) {
       _bloc.add(event);
     }
+  }
+}
+
+class _UtilStateWithBloc<W extends StatefulWidget, S extends BaseState>
+    extends StateWithBloc<W, S> {
+  final BlocBuilder<S> blocBuilder;
+  final WidgetWithStateBuilder<S> widgetBuilder;
+
+  _UtilStateWithBloc(this.blocBuilder, this.widgetBuilder);
+
+  @override
+  Bloc<WidgetEvents, S> createBloc(BuildContext context) {
+    return blocBuilder.call(context);
+  }
+
+  @override
+  Widget createWidgetForState(S state) {
+    return widgetBuilder.call(state);
   }
 }
