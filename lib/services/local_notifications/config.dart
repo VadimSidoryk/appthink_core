4b3705e5a43057506e1b5a058146b51d7aa5f611
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-10-25 16:52:56 +0200

    v 0.0.37: Added local_notification service

---
+++ lib/services/local_notifications/config.dart
@@ -0,0 +1,10 @@
+class LocalNotificationConfig {
+  final String channelId;
+  final String channelName;
+  final String channelDescription;
+
+  LocalNotificationConfig(
+      {required this.channelId,
+      required this.channelName,
+      required this.channelDescription});
+}
