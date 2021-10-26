9ecfa038b0619536f31a269d911108ac3af99ba7
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-10-26 06:32:19 +0200

    v 0.0.38: Added safeCall extension function

--- lib/utils/extentions.dart
+++ lib/utils/extentions.dart
@@ -1,3 +1,18 @@
+import 'either.dart';
+import 'package:applithium_core/logs/extension.dart';
+
 extension ObjectExt<T> on T {
   R let<R>(R Function(T that) op) => op(this);
+}
+
+extension FunctionExt<T> on T Function() {
+  Either<T> safeCall(Object holder, String methodName, [List<Object> params = const []]) {
+    try {
+      holder.logMethod(methodName, params: params);
+      return Either.withValue(this.call())..logResult(holder, methodName);
+    } catch (e, stacktrace) {
+      holder.logError(methodName, ex: e, stacktrace: stacktrace);
+      return Either.withError(e);
+    }
+  }
 }
