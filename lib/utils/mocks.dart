1cb63de371856f327080afc2860275a8086e6c3a
Vadim Sidoryk <lazzyjelly@gmail.com>
2021-11-06 05:23:20 +0100

    v 0.0.42: MockFactory fixes

--- lib/utils/mocks.dart
+++ lib/utils/mocks.dart
@@ -1,91 +1,310 @@
 import 'dart:math';
 
-enum Collection { List, Map, Set }
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 
-class GeneratedParam {
-  final Type forType;
-  final Collection? collectionType;
-  final bool Function(dynamic)? rule;
+abstract class FieldType<T> extends Equatable {
+  Type asDartType();
+}
 
-  GeneratedParam._({required this.forType, required this.rule, required this.collectionType});
+class ObjectType<T> extends FieldType<T> {
+  final Type type;
+
+  @visibleForTesting
+  ObjectType(this.type);
+
+  @override
+  Type asDartType() => type;
+
+  @override
+  List<Object?> get props => [type];
+}
+
+class ListType<T> extends FieldType<List<T>> {
+  final Type itemType;
+
+  @visibleForTesting
+  ListType(this.itemType);
+
+  @override
+  Type asDartType() => List;
+
+  @override
+  List<Object?> get props => [itemType];
+}
+
+class SetType<T> extends FieldType<Set<T>> {
+  final Type itemType;
+
+  @visibleForTesting
+  SetType(this.itemType);
+
+  @override
+  Type asDartType() => Set;
+
+  @override
+  List<Object?> get props => [itemType];
+}
+
+class MapType<K, V> extends FieldType<Map<K, V>> {
+  final Type keyType;
+  final Type valueType;
+
+  @visibleForTesting
+  MapType(this.keyType, this.valueType);
+
+  @override
+  Type asDartType() => Map;
+
+  @override
+  List<Object?> get props => [keyType, valueType];
+}
+
+class FieldToGenerate<T> extends Equatable {
+  final int index;
+  final FieldType<T> type;
+  final bool Function(T)? rule;
+
+  FieldToGenerate._(
+      {required this.index, required this.type, required this.rule});
+
+  @override
+  List<Object?> get props => [index, type];
 }
 
 abstract class _ValueProvider {
-  dynamic getValueFor(Type type);
+  dynamic getValueFor(FieldToGenerate fieldToGenerate);
 }
 
-class _MapValueProvider extends _ValueProvider {
+class _DefaultsValueProvider extends _ValueProvider {
   final Map<Type, dynamic> _map;
 
-  _MapValueProvider(this._map);
+  _DefaultsValueProvider(this._map);
 
   @override
-  getValueFor(Type type) {
-    return _map[type];
+  getValueFor(FieldToGenerate fieldToGenerate) {
+    final type = fieldToGenerate.type.asDartType();
+    final result = _map[type];
+    return result;
   }
 }
 
-class _ListValueProvider extends _ValueProvider {
-  final List<dynamic> _list;
-  var _index = 0;
+class _MockedValueProvider extends _ValueProvider {
+  final Map<FieldToGenerate, dynamic> _values;
 
-  _ListValueProvider(this._list);
+  _MockedValueProvider(this._values);
 
   @override
-  getValueFor(Type type) {
-    return _list[_index++];
+  getValueFor(FieldToGenerate fieldToGenerate) {
+    return _values[fieldToGenerate];
   }
 }
 
-class MockGenerator {
+abstract class MockGenerator {
+  T? nullable<T>({bool Function(T?)? rule});
+
+  T notNull<T>({bool Function(T)? rule});
+
+  List<T?>? nullableList<T>({bool Function(List<dynamic>?)? rule});
+
+  List<T?> notNullList<T>({bool Function(List<dynamic>)? rule});
+
+  Set<T?>? nullableSet<T>({bool Function(Set<dynamic>?)? rule});
+
+  Set<T?> notNullSet<T>({bool Function(Set<dynamic>?)? rule});
+
+  Map<K, V>? nullableMap<K, V>({bool Function(Map<dynamic, dynamic>?)? rule});
+
+  Map<K, V> notNullMap<K, V>({bool Function(Map<dynamic, dynamic>?)? rule});
+}
+
+class EmptyMockGenerator extends MockGenerator {
+  EmptyMockGenerator._();
+
+  @override
+  T notNull<T>({bool Function(T p1)? rule}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  T? nullable<T>({bool Function(T? p1)? rule}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  List<T> notNullList<T>({bool Function(List p1)? rule}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  List<T?>? nullableList<T>({bool Function(List? p1)? rule}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<K, V> notNullMap<K, V>({bool Function(Map? p1)? rule}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Set<T> notNullSet<T>({bool Function(Set? p1)? rule}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<K, V>? nullableMap<K, V>({bool Function(Map? p1)? rule}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Set<T>? nullableSet<T>({bool Function(Set? p1)? rule}) {
+    throw UnimplementedError();
+  }
+}
+
+class MockGeneratorImpl extends MockGenerator {
   final _ValueProvider valueProvider;
-  final Function(GeneratedParam)? onParam;
+  final Function(FieldToGenerate)? onParam;
+  int fieldIndex = 0;
 
-  MockGenerator._({required this.valueProvider, this.onParam});
+  MockGeneratorImpl._(this.valueProvider, {this.onParam});
 
-  T? getWithRule<T>(bool Function(T?)? rule) {
-    onParam?.call(GeneratedParam._(
-      forType: T,
-        rule: rule != null ? ((value) => rule.call(value as T?)) : null,
-        collectionType: null));
-    return valueProvider.getValueFor(T);
+  T? nullable<T>({bool Function(T?)? rule}) {
+    return _getImpl((it) => rule?.call(it) ?? true);
   }
 
-  T? getNullable<T>() {
-    return getWithRule(null);
+  T notNull<T>({bool Function(T)? rule}) {
+    return _getImpl<T>((it) => it != null && (rule?.call(it) ?? true))!;
   }
 
-  T getNotNull<T>() {
-    return getWithRule<T>((it) => it != null)!;
+  T? _getImpl<T>(bool Function(T?)? rule) {
+    final fieldToGenerate = FieldToGenerate._(
+        index: fieldIndex++,
+        type: ObjectType<T>(T),
+        rule: rule != null ? ((value) => rule.call(value as T?)) : null);
+    onParam?.call(fieldToGenerate);
+    final result = valueProvider.getValueFor(fieldToGenerate);
+    return result;
   }
 
-  List<T> getListWithRule<T>(bool Function(T?)? rule) {
-    onParam?.call(GeneratedParam._(
-      forType: T,
-        rule: rule != null ? ((value) => rule.call(value as T?)) : null,
-        collectionType: Collection.List));
-    return (valueProvider.getValueFor(List) as List<dynamic>)
-        .map((it) => it as T)
-        .toList();
+  List<T?>? nullableList<T>({bool Function(List<dynamic>?)? rule}) {
+    return _getListImpl<T>((it) => rule?.call(it) ?? true);
   }
 
-  Set<T> getSetWithRule<T>(bool Function(List<T?>?)? rule) {
-    onParam?.call(GeneratedParam._(
-        forType: T,
-        rule: rule != null ? ((value) => rule.call(value as List<T?>?)) : null,
-        collectionType: Collection.Set));
-    return (valueProvider.getValueFor(List) as Set<dynamic>)
-        .map((it) => it as T)
+  List<T?> notNullList<T>({bool Function(List<dynamic>)? rule}) {
+    return _getListImpl<T>((it) => it != null && (rule?.call(it) ?? true))!;
+  }
+
+  List<T?>? _getListImpl<T>(bool Function(List<dynamic>?)? rule) {
+    final fieldToGenerate = FieldToGenerate._(
+        index: fieldIndex++,
+        type: ListType(T),
+        rule: rule != null
+            ? ((value) => rule.call(value as List<dynamic>?))
+            : null);
+    onParam?.call(fieldToGenerate);
+    try {
+      return (valueProvider.getValueFor(fieldToGenerate) as List<dynamic>?)
+          ?.map((it) => it as T?)
+          .toList();
+    } catch(e) {
+      throw e;
+    }
+  }
+
+  Set<T>? nullableSet<T>({bool Function(Set<dynamic>?)? rule}) {
+    return _getSetImpl<T>((it) => rule?.call(it) ?? true);
+  }
+
+  Set<T> notNullSet<T>({bool Function(Set<dynamic>?)? rule}) {
+    return _getSetImpl<T>((it) => it != null && (rule?.call(it) ?? true))!;
+  }
+
+  Set<T>? _getSetImpl<T>(bool Function(Set<T?>?)? rule) {
+    final fieldToGenerate = FieldToGenerate._(
+        index: fieldIndex++,
+        type: SetType(T),
+        rule: rule != null ? ((value) => rule.call(value as Set<T>)) : null);
+    onParam?.call(fieldToGenerate);
+    return (valueProvider.getValueFor(fieldToGenerate) as Set<dynamic>?)
+        ?.map((it) => it as T)
         .toSet();
+  }
+
+  Map<K, V>? nullableMap<K, V>({bool Function(Map<dynamic, dynamic>?)? rule}) {
+    return _getMapImpl<K, V>((it) => rule?.call(it) ?? true);
+  }
+
+  Map<K, V> notNullMap<K, V>({bool Function(Map<dynamic, dynamic>?)? rule}) {
+    return _getMapImpl<K, V>((it) => it != null && (rule?.call(it) ?? true))!;
+  }
+
+  Map<K, V>? _getMapImpl<K, V>(bool Function(Map<K, V>?)? rule) {
+    final fieldToGenerate = FieldToGenerate._(
+        index: fieldIndex++,
+        type: MapType(K, V),
+        rule:
+            rule != null ? ((value) => rule.call(value as Map<K, V>?)) : null);
+    onParam?.call(fieldToGenerate);
+    return (valueProvider.getValueFor(fieldToGenerate) as Map<dynamic, dynamic>?)
+        ?.map((key, value) => MapEntry(key as K, value as V));
   }
 }
 
 class MocksFactory {
   final Map<Type, List<dynamic>> _valuesSet = {
-    bool: [false, true, null],
-    int: [-123123123123, 0, 123144312323, null],
-    double: [-double.maxFinite, 0.0, double.maxFinite, double.infinity, null],
+    bool: [null, false, true],
+    int: [
+      null,
+      -123123123123,
+      -2455,
+      -1233,
+      -123,
+      -12,
+      -3,
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      14,
+      56,
+      120,
+      2000,
+      12000,
+      123144312323
+    ],
+    double: [
+      null,
+      -double.maxFinite,
+      -123123.321,
+      -123.2,
+      -40,
+      2,
+      -10,
+      0.0,
+      double.maxFinite,
+      0.001,
+      0.1,
+      1,
+      1.5,
+      2,
+      2.5,
+      3,
+      3.5,
+      15.5,
+      200.42,
+      2345.2433,
+      double.infinity
+    ],
     String: [
+      null,
       "",
       "S",
       "Short",
@@ -93,11 +312,11 @@ class MocksFactory {
       "With emojis 👻 💀 ☠️ 👽 👾 🤖 🎃 😺",
       "Do not work for corporations. Old corporations were meaningful when their founders were alive, Do not work for corporations. Old corporations were meaningful when their founders were alive, Do not work for corporations. Old corporations were meaningful when their founders were alive, Do not work for corporations. Old corporations were meaningful when their founders were alive,",
       "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-      null
     ]
   };
 
-  final Map<Type, dynamic> _defaultValues = {
+  @visibleForTesting
+  final Map<Type, dynamic> defaultValues = {
     bool: false,
     int: 0,
     double: 0.0,
@@ -107,82 +326,114 @@ class MocksFactory {
     Set: Set()
   };
 
-  final Map<Type, List<GeneratedParam>> _generatorParams = {};
+  final Map<Type, List<FieldToGenerate>> _fieldsToGenerate = {};
   final Map<Type, Function(MockGenerator)> _generators = {};
 
-  void addValues<T>(List<T> values) {
+  void setValues<T>(List<T> values) {
     _valuesSet[T] = values;
   }
 
-  void addGenerator<T>(T Function(MockGenerator) mockGenerator) {
-    List<GeneratedParam> rules = [];
-    final defaultValue = mockGenerator.call(MockGenerator._(
-        valueProvider: _MapValueProvider(_defaultValues),
-        onParam: (rule) {
-          rules.add(rule);
-        }));
-
-    _defaultValues[T] = defaultValue;
-    _generatorParams[T] = rules;
-    _generators[T] = mockGenerator;
+  void addGenerator<T>(T Function(MockGenerator) mocker) {
+    List<FieldToGenerate> fieldToGenerate = [];
+    final defaultValue = mocker.call(MockGeneratorImpl._(
+        _DefaultsValueProvider(defaultValues), onParam: (field) {
+      fieldToGenerate.add(field);
+    }));
+    defaultValues[T] = defaultValue;
+    _fieldsToGenerate[T] = fieldToGenerate;
+    _generators[T] = mocker;
   }
 
-  List<dynamic> listValues(Type type) {
+  List<T?> listAllVariantsOf<T>(Type type) {
     if (_valuesSet.containsKey(type)) {
-      return _valuesSet[type]!;
+      return _valuesSet[type]!.map((item) => item as T?).toList();
     } else if (_generators.containsKey(type)) {
-      final paramValuesRanges = getValuesRange(type);
-
+      final Map<FieldToGenerate, List<T>> paramValuesRanges =
+          getValuesRangeType<T>(type);
       final random = Random();
-      final result = [];
-
-      for (var paramIndex = 0;
-          paramIndex < paramValuesRanges.length;
-          paramIndex++) {
-        for (final targetValue in paramValuesRanges[paramIndex]) {
-          final testingValuesSet = paramValuesRanges
-              .map((value) => value[random.nextInt(value.length - 1)])
-              .toList()
-            ..[paramIndex] = targetValue;
-          result.add(_generators[type]!.call(MockGenerator._(
-              valueProvider: _ListValueProvider(testingValuesSet))));
+      final result = <T?>[null];
+      final fieldsToGenerate = _fieldsToGenerate[type]!;
+      if (fieldsToGenerate.isNotEmpty) {
+        for (FieldToGenerate targetField in fieldsToGenerate) {
+          for (final targetFieldValue in paramValuesRanges[targetField]!) {
+            final Map<FieldToGenerate, dynamic> testingValuesSet =
+                paramValuesRanges.map((key, value) => MapEntry(
+                    key,
+                    value[value.length > 1
+                        ? random.nextInt(value.length - 1)
+                        : 0]))
+                  ..[targetField] = targetFieldValue;
+            result.add(_generators[type]!.call(
+                MockGeneratorImpl._(_MockedValueProvider(testingValuesSet))));
+          }
         }
+      } else {
+        final generator = _generators[type]!;
+        final value = generator.call(EmptyMockGenerator._());
+        result.add(value);
       }
-      _valuesSet[type] = List.from(result)..add(null);
-      return result;
+      _valuesSet[type] = result;
+      return List.from(result.map((item) => item as T?));
     } else {
       throw "Can't find values for type: $type";
     }
   }
 
-  List<dynamic> getValuesRange(Type type) {
-    final result = [];
-    for (GeneratedParam param in _generatorParams[type]!) {
-      final itemValuesRanges;
-      if (param.rule == null) {
-        itemValuesRanges = listValues(param.forType);
+  @visibleForTesting
+  Map<FieldToGenerate, List<T>> getValuesRangeType<T>(Type type) {
+    final result = <FieldToGenerate, List<T>>{};
+    for (FieldToGenerate field in _fieldsToGenerate[type]!) {
+      final range = getValuesRangeForField(field.type);
+      if (field.rule == null) {
+        result[field] = range.map((it) => it as T).toList();
       } else {
-        itemValuesRanges =
-            listValues(param.forType).where(param.rule!).toList();
-      }
-      if (param.collectionType == null) {
-        result.add(itemValuesRanges);
-      } else if (param.collectionType == Collection.List) {
-        result.add([
-          [],
-          [itemValuesRanges[0]],
-          itemValuesRanges
-        ]);
-      } else if (param.collectionType == Collection.Set) {
-        result.add([
-          Set(),
-          Set()..add(itemValuesRanges[0]),
-          Set.from(itemValuesRanges)
-        ]);
-      } else if (param.collectionType == Collection.Map) {
-        result.add([{}]);
+        final List<T> casted = range.map((it) => it as T).toList();
+        result[field] = casted.where(field.rule!).toList();
       }
     }
     return result;
+  }
+
+  @visibleForTesting
+  List<T?> getValuesRangeForField<T>(FieldType fieldType) {
+    if (fieldType is ListType) {
+      final range = listAllVariantsOf(fieldType.itemType);
+      return <T?>[
+        null,
+        [] as T,
+        [null] as T,
+        [range[1]] as T,
+        range as T
+      ];
+    } else if (fieldType is SetType) {
+      final range = listAllVariantsOf(fieldType.itemType);
+      final Set setWithNull = Set()..add(null);
+      final Set setWithOne = Set()..add(range[1]);
+      return <T?>[
+        null,
+        Set() as T,
+        setWithNull as T,
+        setWithOne as T,
+        range.toSet() as T
+      ];
+    } else if (fieldType is MapType) {
+      final keyRange = listAllVariantsOf(fieldType.keyType);
+      final valueRange = listAllVariantsOf(fieldType.valueType);
+
+      final List<MapEntry> entries = keyRange
+          .map((key) => valueRange.map((value) => MapEntry(key, value)))
+          .expand((i) => i)
+          .toList();
+
+      return <T?>[
+        null,
+        {} as T,
+        {null: null} as T,
+        {keyRange[1]: valueRange[1]} as T,
+        Map.fromEntries(entries) as T
+      ];
+    } else {
+      return listAllVariantsOf<T>((fieldType as ObjectType).type);
+    }
   }
 }
---
+++ test/mocks_test.dart
@@ -0,0 +1,164 @@
+import 'package:applithium_core/utils/mocks.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  test("test mock nullable primitive", () {
+    final factory = MocksFactory();
+    final values = factory.listAllVariantsOf(String);
+    print(values);
+  });
+
+  test("test inner function getValuesRangeType", () {
+    final factory = MocksFactory()
+      ..addGenerator(
+          (mocker) => _SimpleClassWithNullableField(mocker.nullable()));
+    final objValues =
+        factory.getValuesRangeForField<String>(ObjectType(String));
+    print("type = ${objValues.runtimeType}");
+    print("value = $objValues");
+    final listValues = factory.getValuesRangeForField(ListType(String));
+    print(listValues);
+    final setValues = factory.getValuesRangeForField(SetType(String));
+    print(setValues);
+    final mapValues = factory.getValuesRangeForField(MapType(int, String));
+    print(mapValues);
+  });
+
+  test("test inner function getValuesRangeType nullable object rule", () {
+    final factory = MocksFactory()
+      ..addGenerator(
+              (mocker) => _SimpleClassWithNullableField(mocker.nullable(rule: (it) => (it?.length ?? 0) > 10)));
+    print(factory.getValuesRangeType(_SimpleClassWithNullableField));
+  });
+
+  test("test inner function getValuesRangeType notNull object rule", () {
+    final factory = MocksFactory()
+      ..addGenerator(
+              (mocker) => _SimpleClassWithNotNullField(mocker.notNull(rule: (it) => it.length > 10)));
+    print(factory.getValuesRangeType(_SimpleClassWithNotNullField));
+  });
+
+  test("test nullable collection rule", () {
+    final factory = MocksFactory()
+      ..addGenerator(
+              (mocker) => _SimpleClassWithNullableCollectionField(mocker.notNullList<String>(rule: (it) => it.length > 0)
+                  .where((element) => element != null)
+                  .map((e) => e as String)
+                  .toList()));
+     print(factory.getValuesRangeType(_SimpleClassWithNullableCollectionField));
+  });
+
+  test("test notNull collection rule", () {
+    final factory = MocksFactory()
+      ..addGenerator(
+              (mocker) => _SimpleClassWithNullableCollectionField(mocker.notNullList<String>(rule: (it) => it.length > 0)
+                  .where((element) => element != null)
+                  .map((e) => e as String)
+                  .toList()));
+    print(factory.getValuesRangeType(_SimpleClassWithNullableCollectionField));
+  });
+
+  test("test inner function getValuesRangeForField :  mock collections of primitives", () {
+    final factory = MocksFactory();
+    final objValues =
+    factory.getValuesRangeForField<String>(ObjectType(String));
+    print("type = ${objValues.runtimeType}");
+    print("value = $objValues");
+    final listValues = factory.getValuesRangeForField(ListType(String));
+    print(listValues);
+    final setValues = factory.getValuesRangeForField(SetType(String));
+    print(setValues);
+    final mapValues = factory.getValuesRangeForField(MapType(int, String));
+    print(mapValues);
+  });
+
+  test("test inner function getValuesRangeForField : ", () {
+    final factory = MocksFactory()
+      ..addGenerator(
+          (mocker) => _SimpleClassWithNullableField(mocker.nullable()))
+      ..addGenerator(
+          (mocker) => _SimpleClassWithTwoNullableFields(mocker.nullable(), mocker.notNullList<String>()
+          .where((element) => element != null)
+          .map((e) => e as String)
+          .toList()))
+      ..addGenerator(
+              (mocker) => _SimpleClassWithNotNullField(mocker.notNull()));
+     // ..addGenerator((mocker) => _SimpleClassWithTwoNotNullFields(mocker.getNotNull(), mocker.getList((List<String?>? list) => list != null)));
+
+    print("one nullable field range = ${factory.getValuesRangeType(_SimpleClassWithNullableField)}");
+    print("two nullable fields range = ${factory.getValuesRangeType(_SimpleClassWithTwoNullableFields)}");
+    print("one notNull field range = ${factory.getValuesRangeType(_SimpleClassWithNotNullField)}");
+
+    // print("two nonNull fields range = ${factory.getValuesRangeType(_SimpleClassWithTwoNotNullFields)}");
+
+  });
+
+  test("test undefined class", () {
+    final factory = MocksFactory();
+    expect(() => factory.listAllVariantsOf(_SimpleClassWithTwoNullableFields),
+        throwsA(isA<String>()));
+  });
+
+  test("test generate class without param", () {
+    final factory = MocksFactory()
+        ..addGenerator((mocker) => DateTime.now());
+    final values = factory.listAllVariantsOf(DateTime);
+    print(values);
+  });
+
+  test("test generate nullable field", () {
+    final factory = MocksFactory()
+      ..addGenerator(
+              (mocker) => _SimpleClassWithNullableField(mocker.nullable()));
+    final values = factory.listAllVariantsOf(_SimpleClassWithNullableField);
+    print(values);
+  });
+
+  test("test generate notNull field", () {
+    final factory = MocksFactory()
+      ..addGenerator(
+              (mocker) => _SimpleClassWithNotNullField(mocker.notNull()));
+     final values = factory.listAllVariantsOf(_SimpleClassWithNotNullField);
+    // print(values);
+  });
+}
+
+class _SimpleClassWithNullableField {
+  final String? nullableField;
+
+  _SimpleClassWithNullableField(this.nullableField);
+}
+
+class _SimpleClassWithNotNullField {
+  final String notNullField;
+
+  _SimpleClassWithNotNullField(this.notNullField);
+}
+
+class _SimpleClassWithNullableCollectionField {
+  final List<String>? nullableCollection;
+
+  _SimpleClassWithNullableCollectionField(this.nullableCollection);
+}
+
+class _SimpleClassWithNotNullCollectionField {
+  final List<String> notNullField;
+
+  _SimpleClassWithNotNullCollectionField(this.notNullField);
+}
+
+class _SimpleClassWithTwoNullableFields {
+  final String? nullableField;
+  final List<String>? nullableCollectionField;
+
+  _SimpleClassWithTwoNullableFields(this.nullableField, this.nullableCollectionField);
+}
+
+class _SimpleClassWithTwoNotNullFields {
+  final String notNullField;
+  final List<String> notNullCollection;
+
+  _SimpleClassWithTwoNotNullFields(this.notNullField, this.notNullCollection);
+
+}
