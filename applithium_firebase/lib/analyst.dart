15140d4c48ec39d37d29dea03238a6c8f87c2383
Vadim Sidoryk <lazzyjelly@gmail.com>
2022-03-12 10:21:30 +0100

    Firebase v0.1.3: fix analytics impl

--- applithium_firebase/lib/analyst.dart
+++ applithium_firebase/lib/analyst.dart
@@ -1,9 +1,7 @@
-import 'package:applithium_core/events/base_event.dart';
 import 'package:applithium_core/services/analytics/analyst.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/widgets.dart';
-import 'package:applithium_core/logs/extension.dart';
 
 
 class FirebaseAnalystImpl extends Analyst {
@@ -17,13 +15,11 @@ class FirebaseAnalystImpl extends Analyst {
 
   @override
   void setUserProperty(String name, Object? value) {
-    logMethod("setUserProperty", params: [name , value]);
     _analytics.setUserProperty(name: name, value: value?.toString());
   }
 
   @override
-  void onEvent(AplEvent event) {
-    logMethod("onNewEvent", params: [event]);
-    _analytics.logEvent(name: event.name, parameters: event.params);
+  void sendEvent(String name, Map<String, Object?>? params) {
+    _analytics.logEvent(name: name, parameters: params);
   }
 }
