38c8d6e5563d98a234d96a4cbf9aad29344ccab8
Vadim Sidoryk <lazzyjelly@gmail.com>
2022-03-29 11:43:03 +0200

    Core v0.1.9: Added snake_case conversion to FreezedEventsScheme

--- lib/events/mapper/scheme_freezed.dart
+++ lib/events/mapper/scheme_freezed.dart
@@ -2,15 +2,31 @@ import 'package:applithium_core/events/base_event.dart';
 import 'package:applithium_core/events/mapper/scheme.dart';
 
 class FreezedEventsScheme extends EventsScheme {
-
   @override
   List<EventData>? mapEventImpl(AplEvent event) {
-    final eventString = event.toString().replaceAll("Instance of ", "").replaceAll("'", "");
-    final eventParts = eventString.split(new RegExp(r'[,():]')).where((it) => it != "").toList();
-    final name = eventParts.isNotEmpty ? eventParts[0] : "";
+    String eventString = event.toString();
+    final pointPosition = eventString.indexOf(".");
+    if (pointPosition > 0) {
+      eventString =
+          eventString.substring(pointPosition + 1, eventString.length);
+    }
+    eventString =
+        eventString.replaceAll("Instance of ", "").replaceAll("'", "");
+
+    final eventParts = eventString
+        .split(new RegExp(r'[,():]'))
+        .where((it) => it != "")
+        .toList();
+
+    String name = eventParts.isNotEmpty ? eventParts[0] : "";
+    name = name
+        .replaceAllMapped(
+            RegExp(r'(?<=[a-z])[A-Z]'), (Match m) => '_' + (m.group(0) ?? ""))
+        .toLowerCase();
+
     final params = <String, Object>{};
-    for(int i = 1; i <= eventParts.length - 2; i+=2) {
-      params[eventParts[i]] = eventParts[i+1];
+    for (int i = 1; i <= eventParts.length - 2; i += 2) {
+      params[eventParts[i]] = eventParts[i + 1];
     }
     return [EventData(name: name, params: params)];
   }
