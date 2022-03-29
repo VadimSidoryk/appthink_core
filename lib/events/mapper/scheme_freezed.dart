a345f348e90cb0e8a30d7e093ed4192b40a7a9eb
Vadim Sidoryk <lazzyjelly@gmail.com>
2022-03-29 08:42:31 +0200

    Core v0.1.8: Fixed event mapper (2)

--- lib/events/mapper/scheme_freezed.dart
+++ lib/events/mapper/scheme_freezed.dart
@@ -5,11 +5,11 @@ class FreezedEventsScheme extends EventsScheme {
 
   @override
   List<EventData>? mapEventImpl(AplEvent event) {
-    final eventString = event.toString();
-    final eventParts = eventString.split(new RegExp(r'[,():]'));
-    final name = eventParts.length > 1 ? eventParts[0] : "";
+    final eventString = event.toString().replaceAll("Instance of ", "").replaceAll("'", "");
+    final eventParts = eventString.split(new RegExp(r'[,():]')).where((it) => it != "").toList();
+    final name = eventParts.isNotEmpty ? eventParts[0] : "";
     final params = <String, Object>{};
-    for(int i = 1; i < eventParts.length - 2; i+=2) {
+    for(int i = 1; i <= eventParts.length - 2; i+=2) {
       params[eventParts[i]] = eventParts[i+1];
     }
     return [EventData(name: name, params: params)];
