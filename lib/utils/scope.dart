8a7c4c0eaa10363ca64103d27960c7dad05c69a1
Vadim Sidoryk <lazzyjelly@gmail.com>
2022-05-08 06:52:02 +0200

    Core v 0.1.12: Add incrementation of user properties to history service

---
+++ lib/utils/scope.dart
@@ -0,0 +1,34 @@
+import 'package:applithium_core/scopes/scope.dart';
+import 'package:applithium_core/scopes/store.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ScopeUtils {
+  ScopeUtils._();
+
+  Future<T?> showDialogInScope<T>({
+    required BuildContext context,
+    required WidgetBuilder builder,
+    bool barrierDismissible = true,
+    Color? barrierColor = Colors.black54,
+    String? barrierLabel,
+    bool useSafeArea = true,
+    bool useRootNavigator = true,
+    RouteSettings? routeSettings,
+  }) {
+    final initialStore = Scope.of(context)?.store ?? Store();
+    return showDialog(
+      context: context,
+      builder: (dialogContext) => Scope(
+          parentContext: dialogContext,
+          builder: builder,
+          store: Store()..extend(initialStore)),
+      barrierDismissible: barrierDismissible,
+      barrierColor: barrierColor,
+      barrierLabel: barrierLabel,
+      useSafeArea: useSafeArea,
+      useRootNavigator: useRootNavigator,
+      routeSettings: routeSettings
+    );
+  }
+}
