a3453fbd34e63f4e4a3881a3905e72bb572a372d
Vadim Sidoryk <lazzyjelly@gmail.com>
2022-12-13 10:51:15 +0100

    Core v 0.1.53: Added Mocked downloader service

--- lib/services/downloader/abs.dart
+++ lib/services/downloader/abs.dart
@@ -1,8 +1,17 @@
 import 'dart:io';
 
+import 'package:applithium_core/utils/extension.dart';
 import 'package:async/async.dart';
 
+enum DownloadStatus {
+    running,
+    failed,
+    success
+}
+
 abstract class DownloaderService {
+
+    static DownloaderService _mocked = _MockedDownloaderService();
 
     Stream<DownloadStatus> observeIsLoaded(String url);
 
@@ -11,8 +20,20 @@ abstract class DownloaderService {
     Future<Result<File>> open(String url);
 }
 
-enum DownloadStatus {
-    running,
-    failed,
-    success
+class _MockedDownloaderService extends DownloaderService {
+  @override
+  Future<Result<void>> download(String url) => safeCall(() {});
+
+  @override
+  Stream<DownloadStatus> observeIsLoaded(String url) {
+      return Stream.value(DownloadStatus.running);
+  }
+
+  @override
+  Future<Result<File>> open(String url)  => safeCall(() {
+      return File("");
+  });
 }
+
+
+
