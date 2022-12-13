8062798ab296058cc20f1c28d8659426c4b0a477
Vadim Sidoryk <lazzyjelly@gmail.com>
2022-12-13 11:33:44 +0100

    Core v 0.1.56: Improved downloader service

--- lib/services/downloader/test_impl.dart
+++ lib/services/downloader/test_impl.dart
@@ -13,6 +13,11 @@ class TestDownloaderService extends DownloaderService {
 
   @override
   Future<Result<void>> download(String url) => safeCall(() async {
+    final isExist = await _checkIfExist(url);
+        if(isExist) {
+          return;
+        }
+
         if (_urlToStatus.containsKey(url)) {
           return;
         }
@@ -28,8 +33,7 @@ class TestDownloaderService extends DownloaderService {
 
   @override
   Stream<DownloadStatus> observeIsLoaded(String url) async* {
-    final path = await _getFilePath(url);
-    final isExist = await File(path).exists();
+    final isExist = await _checkIfExist(url);
     if (isExist) {
       yield DownloadStatus.success;
     } else if (_urlToStatus.containsKey(url)) {
@@ -42,6 +46,7 @@ class TestDownloaderService extends DownloaderService {
   @override
   Future<Result<File>> open(String url) => safeCall(() async {
         if (_urlToStatus.containsKey(url)) {
+          //download in progress
           await _urlToStatus[url]!
               .firstWhere((element) => element == DownloadStatus.success);
         } else {
@@ -56,5 +61,10 @@ class TestDownloaderService extends DownloaderService {
     final fileName = uri.path.split("/").last;
     String dir = (await getTemporaryDirectory()).path;
     return '$dir/$fileName';
+  }
+
+  Future<bool> _checkIfExist(String url) async {
+    final path = await _getFilePath(url);
+    return await File(path).exists();
   }
 }
