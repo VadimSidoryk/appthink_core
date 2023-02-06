a6cc0c0cd7b80d829f616d9b332b7d55bd81bf5f
Vadim Sidoryk <lazzyjelly@gmail.com>
2023-02-06 14:19:28 +0100

    Core v 0.5.1: Make logger final

--- lib/services/logger/abs.dart
+++ lib/services/logger/abs.dart
@@ -1,4 +1,7 @@
 abstract class Logger {
+
+  const Logger();
+
   void log(String tag, String message, {LogLevel level = LogLevel.debug});
 
   void e(String tag, String message, [dynamic ex, StackTrace? stacktrace]);
