c06407178146f341adc8c03d04c644ba8f73ed9c
Vadim Sidoryk <lazzyjelly@gmail.com>
2023-02-06 14:44:14 +0100

    Amplitude v 0.2.1: Update dependencies

--- lib/services/logger/abs.dart
+++ lib/services/logger/abs.dart
@@ -5,6 +5,16 @@ abstract class Logger {
   void log(String tag, String message, {LogLevel level = LogLevel.debug});
 
   void e(String tag, String message, [dynamic ex, StackTrace? stacktrace]);
+
+  StackTrace removeLast(StackTrace trace, int count) {
+    final stackTraceParts = trace.toString().split(RegExp("#d"));
+    final sb = StringBuffer();
+    for(int i = count; i < stackTraceParts.length; i++) {
+      sb.write(stackTraceParts[i]);
+    }
+
+    return StackTrace.fromString(sb.toString());
+  }
 }
 
 enum LogLevel {
